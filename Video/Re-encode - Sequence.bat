38244ae9ec48b2528f9ec68c9887742c148d20e1
GuestJim <30958420+GuestJim@users.noreply.github.com>
2018-08-24 14:52:58 -0400

    Create Re-encode - Sequence.bat
    The file that actually tells FFmpeg what to do.

---
+++ Video/Re-encode - Sequence.bat
@@ -0,0 +1,22 @@
+@echo off
+::echo "%~1"
+
+set folder=Re-encode
+set rate=30M
+
+set input=%~1
+set output=%~2
+set cut=%~3
+::	not strictly needed, but helpful for remembering
+
+if NOT EXIST "%~dp1%folder%" (
+mkdir "%~dp1%folder%"
+)
+
+ffmpeg -i "%~1" %cut% -map 0:v -map 0:a:1 ^
+-vf "setpts=PTS-STARTPTS, format=pix_fmts=yuvj420p" ^
+-c:v libx264 -crf 18 -maxrate %rate% -bufsize %rate% -preset slower ^
+-c:a copy ^
+-movflags faststart "%~dp1%folder%\%~2.mp4"
+
+::pause
