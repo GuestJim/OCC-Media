d5d60176878251f255ba8d491f566c20ba37cf2a
GuestJim <30958420+GuestJim@users.noreply.github.com>
2018-08-24 16:05:35 -0400

    Update Image Rename and Convert.bat

--- Images/Image Rename and Convert.bat
+++ Images/Image Rename and Convert.bat
@@ -2,13 +2,13 @@
 
 set folder=Small
 
-set	name=0
+set prefix=0
 set pad=2
 
 if NOT EXIST "%~dp1%folder%" (
 mkdir "%~dp1%folder%"
 )
 
-magick convert "*_*_*.png" -write "%name%%%0%pad%d.png" -quality 95 -resize "1920x1080>" "%folder%\%name%%%0%pad%d.jpg"
+magick convert "*_*_*.png" -write "%prefix%%%0%pad%d.png" -quality 95 -resize "1920x1080>" "%folder%\%prefix%%%0%pad%d.jpg"
 
 ::pause
