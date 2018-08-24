7c5edd3ae8e5676ca7e7447cf3082f2eb048172c
GuestJim <30958420+GuestJim@users.noreply.github.com>
2018-08-24 15:14:02 -0400

    Update Sequence.bat

--- Video/Sequence.bat
+++ Video/Sequence.bat
@@ -10,7 +10,7 @@ set batloc=E:\Users\Jim\My Videos\FFMPEG Batch files
 set type=Re-encode - Sequence.bat
 
 set input=Review 2018-08-24 13-04-56 Replay
-set output=F1 2018 - Practice Programs - Tires and Fuel
+set output=
 set cut=-ss 00:04:14 -to 00:09:43
 call "%batloc%\%type%" "%vidloc%\%input%.mkv" "%output%" "%cut%"
 
