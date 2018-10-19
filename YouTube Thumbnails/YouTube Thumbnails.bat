2413d3a03e8ffcfce2e85086cd3acd772abb586d
GuestJim <30958420+GuestJim@users.noreply.github.com>
2018-10-19 12:32:43 -0400

    Update YouTube Thumbnails.bat
    Adding the ability to set the height of the output image

--- YouTube Thumbnails/YouTube Thumbnails.bat
+++ YouTube Thumbnails/YouTube Thumbnails.bat
@@ -3,8 +3,9 @@
 pushd %~dp0
 
 set /p num="How Many Parts: "
+set /p hei="Image Height (default 768): "
 
-python "YouTube Thumbnails.py" "%~1" "%~2" %num% "%~dp1
+python "YouTube Thumbnails.py" "%~1" "%~2" "%num%" "%hei%" "%~dp1
 ::	curiously I cannot close for the path
 ::	this will open the script and pass it those three arguments
 ::		full file path and name, file name, file path
