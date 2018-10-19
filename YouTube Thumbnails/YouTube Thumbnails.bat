5a4e7b836a0b7b9630c111b424b5b09ac0ed0470
GuestJim <30958420+GuestJim@users.noreply.github.com>
2018-10-19 12:34:22 -0400

    Update YouTube Thumbnails.bat

--- YouTube Thumbnails/YouTube Thumbnails.bat
+++ YouTube Thumbnails/YouTube Thumbnails.bat
@@ -2,10 +2,13 @@
 
 pushd %~dp0
 
-set /p num="How Many Parts: "
-::	Allows the user to pass a number to the scripts, for how many of the Part # thumbnails to create
+set /p hei="Image Height (default 768): "
+::	Allows the user to pass a number to the scripts, for the height of the output images
+::		the default of 768 is set in the Python file
 
-python "YouTube Thumbnails.py" "%~1" "%~2" %num% "%~dp1
+python "YouTube Thumbnails.py" "%~1" "%~2" "%num%" "%hei%" "%~dp1
+::	it is necessary to surround each variable with quotes, in case any are blank
+
 ::	curiously I cannot close for the path
 ::	this will open the script and pass it those three arguments
 ::		full file path and name, file name, file path
