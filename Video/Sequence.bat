f910a940ef4e17c587130858fc236bea780a3974
GuestJim <30958420+GuestJim@users.noreply.github.com>
2022-09-03 14:22:10 -0400

    Change to using custom functions
    Learned how to make custom functions in Batch scripts, so have done so to build in the different encoding commands I use. This makes the script more portable as it no longer relies on any external scripts.

--- Video/Sequence.bat
+++ Video/Sequence.bat
@@ -1,17 +1,58 @@
 @echo off
 
 set vidloc=%~dp0
-::	this file should be in the same folder as the videos
+set rate264=30M
+set rate265=10M
+set rateAV1=10M
 
-set batloc=%vidloc%
-set batloc=E:\Users\Jim\My Videos\FFMPEG Batch files
-::	can have the FFmpeg instructions int he same folder but better to reference a different, generic location
+set title=
 
-set type=Re-encode - Sequence.bat
-
-set input=Review 2018-08-24 13-04-56 Replay
-set output=
-set cut=-ss 00:04:14 -to 00:09:43
-call "%batloc%\%type%" "%vidloc%\%input%.mkv" "%output%" "%cut%"
+REM CALL :FFcut "INPUT" "-ss 00:00:00 -to 00:09:59" ""
+CALL :FFx264 "INPUT" "-ss 00:00:00 -to 00:09:59" ""
+REM CALL :FFx265 "INPUT" "-ss 00:00:00 -to 00:09:59" ""
+REM CALL :FFav1 "INPUT" "-ss 00:00:00 -to 00:09:59" ""
 
 ::pause
+
+exit
+
+
+:FOLDcheck <OUTfold>
+if NOT EXIST "%~1" mkdir "%~1"
+exit/B 0
+
+:FFcut <input> <cut> <output>
+set folder=Re-encode
+CALL :FOLDcheck %folder%
+ffmpeg -i "%vidloc%%~1" %~2 -map 0:v -map 0:a:1? ^
+-c copy ^
+-movflags faststart "%vidloc%%folder%\%title% - %~3.mp4" -n
+exit /B 0
+
+:FFx264 <input> <cut> <output>
+set folder=Re-encode
+CALL :FOLDcheck %folder%
+ffmpeg -i "%vidloc%%~1" %~2 -map 0:v -map 0:a:1? ^
+-vf "setpts=PTS-STARTPTS, format=pix_fmts=yuvj420p" ^
+-c:v libx264 -crf 18 -maxrate %rate264% -bufsize %rate264% -preset slow ^
+-c:a copy ^
+-movflags faststart "%vidloc%%folder%\%title% - %~3.mp4" -n
+exit /B 0
+
+:FFx265 <input> <cut> <output>
+set folder=Re-encode - x265
+CALL :FOLDcheck %folder%
+ffmpeg -i "%vidloc%%~1" %~2 -map 0:v -map 0:a:1? ^
+-vf "setpts=PTS-STARTPTS, format=pix_fmts=yuvj420p" ^
+-c:v libx265 -crf 18 -maxrate %rate265% -bufsize %rate265% -preset medium ^
+-c:a copy ^
+-movflags faststart "%vidloc%%folder%\%title% - %~3.mp4" -n
+exit /B 0
+
+:FFav1 <input> <cut> <output>
+set folder=Re-encode - AV1
+CALL :FOLDcheck %folder%
+ffmpeg -i "%vidloc%%~1" %~2 -map 0:v -map 0:a:1? ^
+-vf "setpts=PTS-STARTPTS, format=pix_fmts=yuvj420p" ^
+-c:v libsvtav1 -crf 20 -preset 7 -svtav1-params tune=0:mbr=%rateAV1% ^
+-c:a copy "%vidloc%%folder%\%title% - %~3.mkv" -n
